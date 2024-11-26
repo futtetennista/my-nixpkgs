5a5ddf072188c13b954714a97425d94285075ff1
Stefano <futtetennista@pm.me>
2024-11-26 16:04:47 +0000

    Update default.nix

--- nix/vendor/default.nix
+++ nix/vendor/default.nix
@@ -1,8 +1,20 @@
 { callPackage ? pkgs.callPackage
-, pkgs ? import (import ../../sources.nix).nixpkgs-unstable { }
+, pkgs ? import sources.nixpkgs { inherit system; config = {}; overlays = []; },
+, sources ? import ../../npins,
 }:
 
+let
+  postgresql_12 = pkgs.callPackage ./postgresql {
+    postgresql = pkgs.postgresql_12;
+    version = 12;
+  };
+  postgresql_14 = pkgs.callPackage ./postgresql {
+    postgresql = pkgs.postgresql_14;
+    version = 14;
+  };
+in
 {
   miniconda = (callPackage ./miniconda { });
   pantsbuild = (callPackage ./pantsbuild { });
+  inherit postgresql_12 postgresql_14;
 }
